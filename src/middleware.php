c235cb9cd2650769375002bd386b6ef827aa85f0
Luciana Sato <sato.lu@gmail.com>
2018-03-23 06:31:29 -0300

    Adds validation auth token api

--- src/middleware.php
+++ src/middleware.php
@@ -3,6 +3,14 @@
 
 // e.g: $app->add(new \Slim\Csrf\Guard);
 
+$app->add(new \Tuupola\Middleware\JwtAuthentication([
+    'regexp' => '/(.*)/',
+    'header' => 'Authorization',
+    'path' => '/api',
+    'realm' => 'Protected',
+    'secret' => $container['settings']['secretKey']
+]));
+
 $app->add(function($request, $response, $next) {
     $response = $next($request, $response);
     return $response
